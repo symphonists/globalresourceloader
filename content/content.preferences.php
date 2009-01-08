1b5f91766954297272d4e4635359b455167d1878
Rowan Lewis <rowan@pixelcarnage.com>
2009-01-08 16:19:19 +1000

    Initial commit.

---
+++ content/content.preferences.php
@@ -0,0 +1,202 @@
+<?php
+
+	require_once(TOOLKIT . '/class.administrationpage.php');
+	require_once(TOOLKIT . '/class.datasourcemanager.php');	
+	require_once(TOOLKIT . '/class.eventmanager.php');	
+	
+	class contentExtensionGlobalResourceLoaderPreferences extends AdministrationPage {
+		protected $_driver;
+		
+		public function __viewIndex() {
+			$this->_driver = $this->_Parent->ExtensionManager->create('globalresourceloader');
+			$bIsWritable = true;
+			
+		    if (!is_writable(CONFIG)) {
+		        $this->pageAlert('
+					The Symphony configuration file, <code>/manifest/config.php</code>,
+					is not writable. You will not be able to save changes to
+					preferences.', AdministrationPage::PAGE_ALERT_ERROR
+				);
+		        $bIsWritable = false;
+		    }
+			
+			$this->setPageType('form');
+			$this->setTitle('Symphony &ndash; Global Resources');
+			
+			$this->appendSubheading('Global Resources');
+			
+		// Events --------------------------------------------------------
+			
+			$container = new XMLElement('fieldset');
+			$container->setAttribute('class', 'settings');
+			$container->appendChild(
+				new XMLElement('legend', 'Events')
+			);
+			
+			$group = new XMLElement('div');
+			$group->setAttribute('class', 'group');
+			
+			$this->__viewIndexEventNames($group);
+			$this->__viewIndexEventPages($group);
+			
+			$container->appendChild($group);
+			$this->Form->appendChild($container);
+			
+		// Datasources --------------------------------------------------------
+			
+			$container = new XMLElement('fieldset');
+			$container->setAttribute('class', 'settings');
+			$container->appendChild(
+				new XMLElement('legend', 'Datasources')
+			);
+			
+			$group = new XMLElement('div');
+			$group->setAttribute('class', 'group');
+			
+			$this->__viewIndexDSNames($group);
+			$this->__viewIndexDSPages($group);
+			
+			$container->appendChild($group);
+			$this->Form->appendChild($container);
+			
+		//---------------------------------------------------------------------
+			
+			$div = new XMLElement('div');
+			$div->setAttribute('class', 'actions');
+			
+			$attr = array('accesskey' => 's');
+			if (!$bIsWritable) $attr['disabled'] = 'disabled';
+			$div->appendChild(Widget::Input('action[save]', 'Save Changes', 'submit', $attr));
+
+			$this->Form->appendChild($div);
+		}
+		
+	/*-------------------------------------------------------------------------
+		Events:
+	-------------------------------------------------------------------------*/
+		
+		public function __viewIndexEventNames($context) {
+			$EventManager = new EventManager($this->_Parent);
+			$events = $EventManager->listAll();
+			$options = array();
+			
+			foreach ($events as $event) {
+				$selected = $this->_driver->isEventNameSelected($event['handle']);
+				
+				$options[] = array(
+					$event['handle'], $selected, $event['name']
+				);
+			}
+			
+			$section = Widget::Label('Selected');
+			$section->appendChild(Widget::Select(
+				'settings[event-names][]', $options, array(
+					'multiple'	=> 'multiple'
+				)
+			));
+			
+			$context->appendChild($section);
+		}
+		
+		public function __viewIndexEventPages($context) {
+			$pages = $this->_Parent->Database->fetch("
+				SELECT
+					p.*
+				FROM
+					tbl_pages AS p
+				ORDER BY
+					p.sortorder ASC
+			");
+			$options = array();
+			
+			foreach ($pages as $page) {
+				$selected = $this->_driver->isEventPageSelected($page['id']);
+				
+				$options[] = array(
+					$page['id'], $selected, '/' . $this->_Parent->resolvePagePath($page['id'])
+				);
+			}
+			
+			$section = Widget::Label('Excluded Pages');
+			$section->appendChild(Widget::Select(
+				'settings[event-pages][]', $options, array(
+					'multiple'	=> 'multiple'
+				)
+			));
+			
+			$context->appendChild($section);
+		}
+		
+	/*-------------------------------------------------------------------------
+		Datasources:
+	-------------------------------------------------------------------------*/
+		
+		public function __viewIndexDSNames($context) {
+			$DSManager = new DatasourceManager($this->_Parent);
+			$datasources = $DSManager->listAll();
+			$options = array();
+			
+			foreach ($datasources as $datasource) {
+				$selected = $this->_driver->isDSNameSelected($datasource['handle']);
+				
+				$options[] = array(
+					$datasource['handle'], $selected, $datasource['name']
+				);
+			}
+			
+			$section = Widget::Label('Selected');
+			$section->appendChild(Widget::Select(
+				'settings[ds-names][]', $options, array(
+					'multiple'	=> 'multiple'
+				)
+			));
+			
+			$context->appendChild($section);
+		}
+		
+		public function __viewIndexDSPages($context) {
+			$pages = $this->_Parent->Database->fetch("
+				SELECT
+					p.*
+				FROM
+					tbl_pages AS p
+				ORDER BY
+					p.sortorder ASC
+			");
+			$options = array();
+			
+			foreach ($pages as $page) {
+				$selected = $this->_driver->isDSPageSelected($page['id']);
+				
+				$options[] = array(
+					$page['id'], $selected, '/' . $this->_Parent->resolvePagePath($page['id'])
+				);
+			}
+			
+			$section = Widget::Label('Excluded Pages');
+			$section->appendChild(Widget::Select(
+				'settings[ds-pages][]', $options, array(
+					'multiple'	=> 'multiple'
+				)
+			));
+			
+			$context->appendChild($section);
+		}
+		
+		public function __actionIndex() {
+			$settings  = @$_POST['settings'];
+			
+			if (empty($this->_driver)) {
+				$this->_driver = $this->_Parent->ExtensionManager->create('globalresourceloader');
+			}
+			
+			if (@isset($_POST['action']['save'])) {
+				$this->_driver->setEventNames($settings['event-names']);
+				$this->_driver->setEventPages($settings['event-pages']);
+				$this->_driver->setDSNames($settings['ds-names']);
+				$this->_driver->setDSPages($settings['ds-pages']);
+			}
+		}
+	}
+	
+?>
---
+++ extension.driver.php
@@ -0,0 +1,144 @@
+<?php
+	
+	require_once(TOOLKIT . '/class.datasourcemanager.php');	
+	
+	class Extension_GlobalResourceLoader extends Extension {
+	/*-------------------------------------------------------------------------
+		Definition:
+	-------------------------------------------------------------------------*/
+		
+		public function about() {
+			return array(
+				'name'			=> 'Global Resource Loader',
+				'version'		=> '1.001',
+				'release-date'	=> '2009-01-08',
+				'author'		=> array(
+					'name'			=> 'Rowan Lewis',
+					'website'		=> 'http://pixelcarnage.com',
+					'email'			=> 'rowan@pixelcarnage.com'
+				),
+				'description'	=> 'Allows you to select datasources and events to be used on all pages.'
+	 		);
+		}
+		
+		public function uninstall() {
+			$this->_Parent->Configuration->remove('globalresourceloader');
+			$this->_Parent->saveConfig();
+		}
+		
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/frontend/',
+					'delegate'	=> 'FrontendPageResolved',
+					'callback'	=> 'manipulatePageData'
+				),
+				array(
+					'page'		=> '/system/preferences/',
+					'delegate'	=> 'AddCustomPreferenceFieldsets',
+					'callback'	=> 'preferences'
+				)
+			);
+		}
+		
+		public function fetchNavigation() {
+			return array(
+				array(
+					'location'	=> 200,
+					'name'	=> 'Globals',
+					'link'	=> '/preferences/'
+				)
+			);
+		}
+		
+		public function manipulatePageData($context) {
+			$datasources = $context['page_data']['data_sources'];
+			$datasources = explode(',', $datasources);
+			$datasources = array_merge($datasources, $this->getDatasources());
+			$datasources = array_unique($datasources);
+			$datasources = implode(',', $datasources);
+			
+			// Ignore excluded pages:
+			if (!$this->isPageSelected($context['page_data']['id'])) {
+				$context['page_data']['data_sources'] = $datasources;
+			}
+		}
+		
+	/*-------------------------------------------------------------------------
+		Events:
+	-------------------------------------------------------------------------*/
+		
+		public function getEventNames() {
+			return explode(',', $this->_Parent->Configuration->get('event-names', 'globalresourceloader'));
+		}
+		
+		public function getEventPages() {
+			return explode(',', $this->_Parent->Configuration->get('event-pages', 'globalresourceloader'));
+		}
+		
+		public function setEventNames($events) {
+			$events = implode(',', $events);
+			
+			$this->_Parent->Configuration->set('event-names', $events, 'globalresourceloader');
+			$this->_Parent->saveConfig();
+		}
+		
+		public function setEventPages($pages) {
+			$pages = implode(',', $pages);
+			
+			$this->_Parent->Configuration->set('event-pages', $pages, 'globalresourceloader');
+			$this->_Parent->saveConfig();
+		}
+		
+		public function isEventNameSelected($handle) {
+			$datasources = $this->getEventNames();
+			
+			return in_array($handle, $datasources);
+		}
+		
+		public function isEventPageSelected($id) {
+			$pages = $this->getEventPages();
+			
+			return in_array($id, $pages);
+		}
+		
+	/*-------------------------------------------------------------------------
+		Datasources:
+	-------------------------------------------------------------------------*/
+		
+		public function getDSNames() {
+			return explode(',', $this->_Parent->Configuration->get('ds-names', 'globalresourceloader'));
+		}
+		
+		public function getDSPages() {
+			return explode(',', $this->_Parent->Configuration->get('ds-pages', 'globalresourceloader'));
+		}
+		
+		public function setDSNames($datasources) {
+			$datasources = implode(',', $datasources);
+			
+			$this->_Parent->Configuration->set('ds-names', $datasources, 'globalresourceloader');
+			$this->_Parent->saveConfig();
+		}
+		
+		public function setDSPages($pages) {
+			$pages = implode(',', $pages);
+			
+			$this->_Parent->Configuration->set('ds-pages', $pages, 'globalresourceloader');
+			$this->_Parent->saveConfig();
+		}
+		
+		public function isDSNameSelected($handle) {
+			$datasources = $this->getDSNames();
+			
+			return in_array($handle, $datasources);
+		}
+		
+		public function isDSPageSelected($id) {
+			$pages = $this->getDSPages();
+			
+			return in_array($id, $pages);
+		}
+	}
+	
+?>
