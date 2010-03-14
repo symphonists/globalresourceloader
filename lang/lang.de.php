f6d2d4a2a8c3dd578d33b5ad9335b840363ce35e
Nils Werner <nils.werner@gmail.com>
2010-03-14 12:00:34 +0100

    German translation

---
+++ lang/lang.de.php
@@ -0,0 +1,40 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Nils Werner',
+			'email' => 'nils.werner@gmail.com',
+			'website' => 'http://www.phoque.de'
+		),
+		'release-date' => '2010-03-14'
+	);
+	
+	
+	/*
+	 * EXTENSION: Export Ensemble
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'Globals' =>
+		'Globales',
+
+		'Global Resources' =>
+		'Globale Komponenten',
+		
+		'Selected' =>
+		'Ausgewählte',
+		
+		'Excluded Pages' =>
+		'Ausgenommene Seiten',
+
+		'The Symphony configuration file, <code>/manifest/config.php</code>,
+		is not writable. You will not be able to save changes to
+		preferences.' =>
+		'Die Symphony-Konfigurationsdatei <code>/manifest/config.php</code> kann nicht geändert werden.
+		Ihre Einstellungen werden nicht gespeichert.',
+
+	);
+	
